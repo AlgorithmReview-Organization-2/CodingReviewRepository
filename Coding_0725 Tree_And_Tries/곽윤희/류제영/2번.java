5c9dcfd68038c63b9ffdec3b32646ade6932356c
JeYeongR <113500815+JeYeongR@users.noreply.github.com>
2023-08-01 18:44:31 +0900

    Create 2번.java

---
+++ Coding_0725 Tree_And_Tries/곽윤희/류제영/2번.java
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/root-equals-sum-of-children/
+
+'''
+1. 아이디어 :
+    자식들을 더함
+2. 시간복잡도 :
+    O(n)
+3. 자료구조 :
+    트리
+'''
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean checkTree(TreeNode root) {
+        return root.val == root.left.val + root.right.val;
+    }
+}
