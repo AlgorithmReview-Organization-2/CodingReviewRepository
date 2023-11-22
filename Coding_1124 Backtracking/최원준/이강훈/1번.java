be9f3aa854764f2cde9738c8550fc99ad9cec27c
Hoonii2 <lkh66913@naver.com>
2023-11-22 19:11:24 +0900

    docs: 6주차 수정

--- Coding_1124 Backtracking/최원준/이강훈/1번.java
+++ Coding_1124 Backtracking/최원준/이강훈/1번.java
@@ -11,37 +11,13 @@ https://leetcode.com/problems/find-unique-binary-string/description/
 O(N)
 */
 
-import java.util.List;
-
 public class Solution {
-    int max = 0;
+    public String findDifferentBinaryString(String[] nums) {
+        StringBuilder sb = new StringBuilder();
 
-    public int maxLength(List<String> arr) {
-        backTrack(arr, "", 0);
-
-        return max;
-    }
-
-    private void backTrack(List<String> arr, String current, int start) {
-        if (max < current.length()) max = current.length();
-
-        for (int i = start; i < arr.size(); i++) {
-            if (!isValid(current, arr.get(i))) continue;
-
-            backTrack(arr, current + arr.get(i), i + 1);
+        for (int i = 0; i < nums.length; i++) {
+            sb.append(nums[i].charAt(i) == '0' ? "1" : "0");
         }
-    }
-
-    private boolean isValid(String currentString, String newString) {
-        int[] array = new int[26];
-
-        for (int i = 0; i < newString.length(); i++) {
-            if (++array[newString.charAt(i) - 'a'] == 2)
-                return false;
-
-            if (currentString.contains(newString.charAt(i) + ""))
-                return false;
-        }
-        return true;
+        return new String(sb);
     }
 }
