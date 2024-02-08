d1d85e85f2480ca8e5a7ff5071c921f49546bc16
jujemu <jujemu30@gmail.com>
2024-02-09 08:12:04 +0900

    Graph 1번 문제, 여행 가자; 플로이드 워셜 풀이 추가

--- Coding_240207 Graph/주제무/주제무/1.py
+++ Coding_240207 Graph/주제무/주제무/1.py
@@ -1,7 +1,46 @@
 """
 https://www.acmicpc.net/problem/1976
 
+구현
+- 플로이드 워셜 풀이
+- 서로소 집합 풀이
+
+서로소 풀이가 깔끔하고 좋네요
 """
+N, M = map(int, [input() for _ in range(2)])
+
+INF = int(1e9)
+adj = []
+for i in range(N):
+    s = list(map(int, input().split()))
+    for j in range(N):
+        if i == j:
+            s[j] = 1
+            continue
+
+        if s[j] == 0:
+            s[j] = INF
+    adj.append(s)
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j])
+
+plan = [*map(lambda x: int(x)-1, input().split())]
+
+result = True
+pre_city = plan[0]
+for cur_city in plan[1:]:
+    if adj[pre_city][cur_city] >= INF:
+        result = False
+        break
+
+    pre_city = cur_city
+
+print("YES" if result else "NO")
+
+""" disjoint-set 풀이
 N, M = map(int, [input() for _ in range(2)])
 parent = [i for i in range(N)]
 adj = [list(map(int, input().split())) for _ in range(N)]
@@ -34,3 +73,4 @@ for city in plan[1:]:
         break
 
 print("YES" if result else "NO")
+"""
